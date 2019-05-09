12159b50ed1893108c2601fe6b4d4d67a46ff12e
yufangtongqin <48903568+yufangtongqin@users.noreply.github.com>
2019-05-09 09:43:07 +0800

    Copy
    Class

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,49 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+class CTestCopy
+{
+private:
+	int x;
+	int y;
+public:
+	CTestCopy();
+	CTestCopy(int x, int y);
+	void display();
+	void setcopy();
+};
+CTestCopy;; CTestCopy()
+{
+
+}
+CTestCopy::CTestCopy(int a, int b);
+{
+	x = a;
+	y = b;
+}
+CTestCopy;; CTestCopy(CTestCopy(&om))
+{
+	x = omx * 2;
+	y = omy * 2;
+	cout << "************************" << endl;
+}
+void CTestCopy;; setcopy(int a, int b)
+{
+	x = a;
+	y = b;
+}
+void mytest(CTestCopy temp)
+{
+	CTestCopy temp;
+	display();
+}
+int main()
+{
+	CTestCopy ocpy(100, 200);
+	ocpy display();
+	void setCopy(int a, int b);
+	return 0;
+}
